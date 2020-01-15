1ff5a62e6ce28c2e16d0def49dfe862678a0ede7
ymon-oo <40437513@live.napier.ac.uk>
2020-01-15 13:45:25 +0630

    Lab5

--- src/main/java/com/napier/sem1/App.java
+++ src/main/java/com/napier/sem1/App.java
@@ -182,7 +182,7 @@ public class App
 
         // Extract employee salary information
         ArrayList<Employee> employees = a.getAllSalaries();
-
+a.printSalaries(employees);
         // Test the size of the returned data - should be 240124
         System.out.println(employees.size());
 
