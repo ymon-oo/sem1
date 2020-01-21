dfb29bcd26d527dc4299b542459194e4d51105f2
ymon-oo <40437513@live.napier.ac.uk>
2020-01-21 14:58:27 +0630

    Create test.java for unit test

---
+++ src/main/java/com/napier/sem1/Department.java
@@ -0,0 +1,18 @@
+package com.napier.sem1;
+
+public class Department {
+    /**
+     * Department number
+     */
+    public int dep_no;
+
+    /**
+     * Department's name
+     */
+    public String dep_name;
+    /**
+     * manager's name
+     */
+    public String manag_name;
+
+}
